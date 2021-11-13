cc3cdc3bd407227a74e4363b94d076ad9a7fba40
NourG8 <nourgerfali08@gmail.com>
2021-11-13 22:13:38 +0100

    Add _FrontLayer class

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -26,6 +26,34 @@ class Backdrop extends StatefulWidget {
 }
 
 // TODO: Add _FrontLayer class (104)
+class _FrontLayer extends StatelessWidget {
+  // TODO: Add on-tap callback (104)
+  const _FrontLayer({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 16.0,
+      shape: const BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(46.0)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // TODO: Add a GestureDetector (104)
+          Expanded(
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
 // TODO: Add _BackdropTitle class (104)
 // TODO: Add _BackdropState class (104)
 class _BackdropState extends State<Backdrop>
